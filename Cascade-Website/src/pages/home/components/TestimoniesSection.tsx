b9f5a9071fb4cec41a0e2aec494fa7f07961f820
Ediomojose <goodnewsediomojoshua@yahoo.com>
2024-03-08 21:53:59 +0100

    Changes

--- Cascade-Website/src/pages/home/components/TestimoniesSection.tsx
+++ Cascade-Website/src/pages/home/components/TestimoniesSection.tsx
@@ -6,107 +6,163 @@ const TestimoniesSection = () => {
   return (
     <>
       <section>
-        <article className={`grid grid-cols-1 justify-center items-center gap-10 py-12 `}>
-          <div className={`flex items-center justify-center px-5 md:px-10 lg:px-20`}>
+        <article
+          className={`grid grid-cols-1 justify-center items-center gap-10 py-12 `}
+        >
+          <div
+            className={`flex items-center justify-center px-5 md:px-10 lg:px-20`}
+          >
             <div>
-              <p className={`text-brandBlue1x font-valueBold py-3`}>Sneak Peak - From Techies who started their journey early!</p>
-              <h2 className={`text-4xl font-DegularDiplayBold pb-2 md:text-5xl lg:text-6xl`}>
+              <p className={`text-brandBlue1x font-valueBold py-3`}>
+                Sneak Peak - From Techies who started their journey early!
+              </p>
+              <h2
+                className={`text-4xl font-DegularDiplayBold pb-2 md:text-5xl lg:text-6xl`}
+              >
                 Why it is best to start <br /> early?
               </h2>
             </div>
           </div>
-          <section className={`flex items-center  py-4 mx-5   gap-3 overflow-x-scroll snap-mandatory snap-x no-scrollbar lg:px-20 lg:mx-10`}>
-              
-          <div className={`grid grid-cols-1 gap-4 justify-center items-center w-full  shrink-0   bg-brandWhite1x/80 p-5 px-10 rounded-xl shadow-xl md:w-2/5`}>
-                <p className={`text-6xl font-euclidItalicBold`}>"</p>
-                <div className={`flex gap-4`}>
-                  <img src={Profile} alt="Cascade: Profile images" className={`w-[15%] h-[15%] border-2  border-brandBlue1x rounded-full`}/>
-                  <div >
-                    <p className={`text-md font-valueBold`}>Henry Agu</p>
-                    <p className={`text-xxs font-euclidItalic md:text-xs`}>Software Developer</p>
-                    <p className={`text-xxs  font-euclidItalic md:text-xs`}>Twitter handle</p>
-                  </div>
-                </div>
-                <p className={`text-xs font-euclidRegular lg:text-sm`}>
-                  Just a story of a young individual who had primary education
-                  of tech and how it sharped his entire career. Just a story of
-                  a young individual who had primary education of tech and how
-                  it sharped his entire career. Just a story of a young
-                  individual who had primary education of tech.
-                </p>
-              </div>
-              <div className={`grid grid-cols-1 gap-4 justify-center items-center   w-full  shrink-0   bg-brandWhite1x/80 p-5 px-10 rounded-xl shadow-xl md:w-2/5`}>
-                <p className={`text-6xl font-euclidItalicBold`}>"</p>
-                <div className={`flex gap-4`}>
-                  <img src={Profile} alt="Cascade: Profile images" className={`w-[15%] h-[15%] border-2  border-brandBlue1x rounded-full`}/>
-                  <div >
+          <section
+            className={`flex items-center  py-4 mx-5   gap-3 overflow-x-scroll snap-mandatory snap-x no-scrollbar lg:px-20 lg:mx-10`}
+          >
+            <div
+              className={`grid grid-cols-1 gap-4 justify-center items-center w-full  shrink-0   bg-brandWhite1x/80 p-5 px-10 rounded-xl shadow-xl md:w-2/5`}
+            >
+              <p className={`text-6xl font-euclidItalicBold`}>"</p>
+              <div className={`flex gap-4`}>
+                <img
+                  src={Profile}
+                  alt="Cascade: Profile images"
+                  className={`w-[15%] h-[15%] border-2  border-brandBlue1x rounded-full`}
+                />
+                <div>
                   <p className={`text-md font-valueBold`}>Henry Agu</p>
-                    <p className={`text-xxs font-euclidItalic md:text-xs`}>Software Developer</p>
-                    <p className={`text-xxs  font-euclidItalic md:text-xs`}>Twitter handle</p>
-                  </div>
+                  <p className={`text-xxs font-euclidItalic md:text-xs`}>
+                    Software Developer
+                  </p>
+                  <p className={`text-xxs  font-euclidItalic md:text-xs`}>
+                    Twitter handle
+                  </p>
                 </div>
-                <p className={`text-xs font-euclidRegular lg:text-sm`}>
-                  Just a story of a young individual who had primary education
-                  of tech and how it sharped his entire career. Just a story of
-                  a young individual who had primary education of tech and how
-                  it sharped his entire career. Just a story of a young
-                  individual who had primary education of tech.
-                </p>
               </div>
-              <div className={`grid grid-cols-1 gap-4 justify-center items-center   w-full  shrink-0   bg-brandWhite1x/80 p-5 px-10 rounded-xl shadow-xl md:w-2/5`}>
-                <p className={`text-6xl font-euclidItalicBold`}>"</p>
-                <div className={`flex gap-4`}>
-                  <img src={Profile} alt="Cascade: Profile images" className={`w-[15%] h-[15%] border-2  border-brandBlue1x rounded-full`}/>
-                  <div >
+              <p className={`text-xs font-euclidRegular lg:text-sm`}>
+                Starting early laid the foundation for my tech career. As a
+                teenager, diving into coding and robotics sparked my passion for
+                technology, setting me on a trajectory towards success in the
+                industry.
+              </p>
+            </div>
+            <div
+              className={`grid grid-cols-1 gap-4 justify-center items-center   w-full  shrink-0   bg-brandWhite1x/80 p-5 px-10 rounded-xl shadow-xl md:w-2/5`}
+            >
+              <p className={`text-6xl font-euclidItalicBold`}>"</p>
+              <div className={`flex gap-4`}>
+                <img
+                  src={Profile}
+                  alt="Cascade: Profile images"
+                  className={`w-[15%] h-[15%] border-2  border-brandBlue1x rounded-full`}
+                />
+                <div>
                   <p className={`text-md font-valueBold`}>Henry Agu</p>
-                    <p className={`text-xxs font-euclidItalic md:text-xs`}>Software Developer</p>
-                    <p className={`text-xxs  font-euclidItalic md:text-xs`}>Twitter handle</p>
-                  </div>
+                  <p className={`text-xxs font-euclidItalic md:text-xs`}>
+                    Software Developer
+                  </p>
+                  <p className={`text-xxs  font-euclidItalic md:text-xs`}>
+                    Twitter handle
+                  </p>
                 </div>
-                <p className={`text-xs font-euclidRegular lg:text-sm`}>
-                  Just a story of a young individual who had primary education
-                  of tech and how it sharped his entire career. Just a story of
-                  a young individual who had primary education of tech and how
-                  it sharped his entire career. Just a story of a young
-                  individual who had primary education of tech.
-                </p>
               </div>
-              <div className={`grid grid-cols-1 gap-4 justify-center items-center   w-full  shrink-0   bg-brandWhite1x/80 p-5 px-10 rounded-xl shadow-xl md:w-2/5`}>
-                <p className={`text-6xl font-euclidItalicBold`}>"</p>
-                <div className={`flex gap-4`}>
-                  <img src={Profile} alt="Cascade: Profile images" className={`w-[15%] h-[15%] border-2  border-brandBlue1x rounded-full`}/>
-                  <div >
+              <p className={`text-xs font-euclidRegular lg:text-sm`}>
+                Early exposure to technology opened doors I never imagined. From
+                building my first website in middle school to developing apps in
+                high school, those early experiences equipped me with the skills
+                and confidence to thrive in the tech world
+              </p>
+            </div>
+            <div
+              className={`grid grid-cols-1 gap-4 justify-center items-center   w-full  shrink-0   bg-brandWhite1x/80 p-5 px-10 rounded-xl shadow-xl md:w-2/5`}
+            >
+              <p className={`text-6xl font-euclidItalicBold`}>"</p>
+              <div className={`flex gap-4`}>
+                <img
+                  src={Profile}
+                  alt="Cascade: Profile images"
+                  className={`w-[15%] h-[15%] border-2  border-brandBlue1x rounded-full`}
+                />
+                <div>
                   <p className={`text-md font-valueBold`}>Henry Agu</p>
-                    <p className={`text-xxs font-euclidItalic md:text-xs`}>Software Developer</p>
-                    <p className={`text-xxs  font-euclidItalic md:text-xs`}>Twitter handle</p>
-                  </div>
+                  <p className={`text-xxs font-euclidItalic md:text-xs`}>
+                    Software Developer
+                  </p>
+                  <p className={`text-xxs  font-euclidItalic md:text-xs`}>
+                    Twitter handle
+                  </p>
                 </div>
-                <p className={`text-xs font-euclidRegular lg:text-sm`}>
-                  Just a story of a young individual who had primary education
-                  of tech and how it sharped his entire career. Just a story of
-                  a young individual who had primary education of tech and how
-                  it sharped his entire career. Just a story of a young
-                  individual who had primary education of tech.
-                </p>
               </div>
-              <div className={`grid grid-cols-1 gap-4 justify-center items-center   w-full  shrink-0   bg-brandWhite1x/80 p-5 px-10 rounded-xl shadow-xl md:w-2/5`}>
-                <p className={`text-6xl font-euclidItalicBold`}>"</p>
-                <div className={`flex gap-4`}>
-                  <img src={Profile} alt="Cascade: Profile images" className={`w-[15%] h-[15%] border-2  border-brandBlue1x rounded-full`}/>
-                  <div >
+              <p className={`text-xs font-euclidRegular lg:text-sm`}>
+                Beginning my tech journey early gave me a competitive edge. By
+                the time I entered college, I had already interned at tech
+                companies and contributed to open-source projects, positioning
+                myself as a standout candidate for future opportunities
+              </p>
+            </div>
+            <div
+              className={`grid grid-cols-1 gap-4 justify-center items-center   w-full  shrink-0   bg-brandWhite1x/80 p-5 px-10 rounded-xl shadow-xl md:w-2/5`}
+            >
+              <p className={`text-6xl font-euclidItalicBold`}>"</p>
+              <div className={`flex gap-4`}>
+                <img
+                  src={Profile}
+                  alt="Cascade: Profile images"
+                  className={`w-[15%] h-[15%] border-2  border-brandBlue1x rounded-full`}
+                />
+                <div>
                   <p className={`text-md font-valueBold`}>Henry Agu</p>
-                    <p className={`text-xxs font-euclidItalic md:text-xs`}>Software Developer</p>
-                    <p className={`text-xxs  font-euclidItalic md:text-xs`}>Twitter handle</p>
-                  </div>
+                  <p className={`text-xxs font-euclidItalic md:text-xs`}>
+                    Software Developer
+                  </p>
+                  <p className={`text-xxs  font-euclidItalic md:text-xs`}>
+                    Twitter handle
+                  </p>
                 </div>
-                <p className={`text-xs font-euclidRegular lg:text-sm`}>
-                  Just a story of a young individual who had primary education
-                  of tech and how it sharped his entire career. Just a story of
-                  a young individual who had primary education of tech and how
-                  it sharped his entire career. Just a story of a young
-                  individual who had primary education of tech.
-                </p>
               </div>
+              <p className={`text-xs font-euclidRegular lg:text-sm`}>
+                Starting early allowed me to make mistakes and learn from them.
+                As a young entrepreneur experimenting with different tech
+                ventures, I gained invaluable insights and resilience that
+                continue to shape my approach to innovation and
+                problem-solving..
+              </p>
+            </div>
+            <div
+              className={`grid grid-cols-1 gap-4 justify-center items-center   w-full  shrink-0   bg-brandWhite1x/80 p-5 px-10 rounded-xl shadow-xl md:w-2/5`}
+            >
+              <p className={`text-6xl font-euclidItalicBold`}>"</p>
+              <div className={`flex gap-4`}>
+                <img
+                  src={Profile}
+                  alt="Cascade: Profile images"
+                  className={`w-[15%] h-[15%] border-2  border-brandBlue1x rounded-full`}
+                />
+                <div>
+                  <p className={`text-md font-valueBold`}>Henry Agu</p>
+                  <p className={`text-xxs font-euclidItalic md:text-xs`}>
+                    Software Developer
+                  </p>
+                  <p className={`text-xxs  font-euclidItalic md:text-xs`}>
+                    Twitter handle
+                  </p>
+                </div>
+              </div>
+              <p className={`text-xs font-euclidRegular lg:text-sm`}>
+                Early immersion in tech cultivated a lifelong passion for
+                learning. From tinkering with hardware to exploring the latest
+                programming languages, my journey from a young age instilled in
+                me a curiosity and hunger for knowledge that drives me forward
+                every day.
+              </p>
+            </div>
           </section>
         </article>
       </section>
